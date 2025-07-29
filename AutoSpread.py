9ec433cf16c2421bd4d5dd9ea8a6a88e293defa8
Doctor Mkhonza <163717216+LoelStark@users.noreply.github.com>
2025-07-29 06:03:09 -0400

    Add files via upload

---
+++ AutoSpread.py
@@ -0,0 +1,27 @@
+#Script will remain incomplete due to further analysis
+#SO, again this is for educational purposes only,
+
+import getpass
+import uuid
+
+def main():
+	local_script_path = os.path.realpath(_file_)
+	remote_file_filename = "spread.py"
+#You can use any file name you desire
+
+
+	username = input("enter your username: ")
+`	password = getpass.getpass("passowrd ")
+ 	
+	taqrgets = scan_subnet()
+
+#Due to the script being unfinished, I thought of incoperating a for loop below this line
+	
+	#for ip in targets
+
+#AN Auto spread logic is an automated way of ditributing a total quantity (points, resources, money, etc...)
+#Across multiple targets based on specific rules or constraints such as equal share weights.
+
+**********************************IN simple terms***************************************************
+
+#Its a rule nased system that automatically allocates a total value to several recipients without manual calculation!
